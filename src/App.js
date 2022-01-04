2aab37f6378f02b5389cd39bf18ff2fb0819dc67
TPavZ <tmpavlic@gmail.com>
2022-01-04 14:16:23 -0600

    Added addEntryForm component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 
 function App() {
 
-  const [entries, setEntries] = useState([{weight: 135, date: "01/04/2022"}, {weight: 137, date: "01/05/2022"}])
+  const [entries, setEntries] = useState([{ weight: 135, date: "01-04-2022" }, { weight: 137, date: "01-05-2022" }])
+
+  function addNewEntry(entry) {
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
 
   return (
     <div>
-      <DisplayEntries parentEntries={entries}/>
+      <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntry={addNewEntry} />
     </div>
   );
 }
